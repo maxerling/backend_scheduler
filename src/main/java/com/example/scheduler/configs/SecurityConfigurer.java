af65ed9ecdd5a16405d6be9acfe1f3bca5a4ebf0
Max Erling <max.d.erling@gmail.com>
2021-11-28 16:36:16 +0100

    change authorization

--- src/main/java/com/example/scheduler/configs/SecurityConfigurer.java
+++ src/main/java/com/example/scheduler/configs/SecurityConfigurer.java
@@ -36,55 +36,36 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-      /*  http.csrf().disable()
-                .authorizeRequests()
-                .antMatchers("/auth")
-                .permitAll()
-               .anyRequest()
-               .authenticated();*/
-//        http.cors();
-//        http.csrf().disable().authorizeRequests()
-//                .antMatchers("/admin").hasRole("ADMIN")
-//                .antMatchers("/user").hasAnyRole("USER", "ADMIN")
-//                .antMatchers("/scheduler").hasAnyRole("USER", "ADMIN")
-//                .antMatchers("/").authenticated()
-//                .antMatchers("/auth").permitAll().anyRequest().authenticated()
-//                .and().sessionManagement()
-//                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-
-//        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
-//                .and()
-//                .formLogin()
-//                .loginPage("/login").permitAll();
 
         http.cors();
-        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-                .csrf().disable().authorizeRequests((request) -> request.antMatchers("/user").hasRole("USER")
-                        .antMatchers("/scheduler").hasRole("USER")
-                        .antMatchers("/admin").hasRole("ADMIN")
-                        .antMatchers("/").permitAll()
-                        .antMatchers("/login").permitAll()
-                        .antMatchers("/auth").permitAll()
-                        .antMatchers("/user/add").permitAll()
-                        .anyRequest().authenticated()
-                )
-                .addFilterBefore(new JwtRequestFilter(UserDetailsService,jwtUtil),UsernamePasswordAuthenticationFilter.class);
-//        http.formLogin()
-//                .loginPage("/login")
-//                .loginProcessingUrl("/auth")
-//                .defaultSuccessUrl("/scheduler.html")
-//                .failureUrl("/login.html?error=true");
-        //http.formLogin();
+        http.csrf().disable().authorizeRequests()
+                .antMatchers("/user").hasRole("USER")
+                .antMatchers("/scheduler").hasRole("USER")
+                .antMatchers("/").permitAll()
+                .antMatchers("/login").permitAll()
+                .antMatchers("/user/add").permitAll()
+                .antMatchers("/auth").permitAll().anyRequest().authenticated()
+                .and().sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+
 
 //        http.cors();
-//        http.csrf().disable().headers().frameOptions().disable();
+//        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+//                .csrf().disable().authorizeRequests((request) ->
+//                        request
+//
+//                                .antMatchers("/user").hasRole("USER")
+//                        .antMatchers("/scheduler").hasRole("USER")
+//                        .antMatchers("/admin").hasRole("ADMIN")
+//                        .antMatchers("/").permitAll()
+//                        .antMatchers("/login").permitAll()
+//                        .antMatchers("/auth").permitAll()
+//                        .antMatchers("/user/add").permitAll()
+//                        .anyRequest()
+//                )
+//                .addFilterBefore(new JwtRequestFilter(UserDetailsService,jwtUtil),UsernamePasswordAuthenticationFilter.class);
 
-//        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().exceptionHandling()
-//                .authenticationEntryPoint(authenticationEntryPoint).and()
-//                .authorizeRequests((request) -> request.antMatchers("/h2-console/**", "/api/v1/auth/login").permitAll()
-//                        .antMatchers(HttpMethod.OPTIONS, "/**").permitAll().anyRequest().authenticated())
-//                .addFilterBefore(new JWTAuthenticationFilter(userService, jWTTokenHelper),
-//                        UsernamePasswordAuthenticationFilter.class);
 
     }
 
