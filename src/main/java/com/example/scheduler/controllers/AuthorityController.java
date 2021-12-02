d69d85640c83cbe09daabb3e2f01473487f0e9cf
Max Erling <max.d.erling@gmail.com>
2021-12-02 21:24:37 +0100

    fix authority controller

--- src/main/java/com/example/scheduler/controllers/AuthorityController.java
+++ src/main/java/com/example/scheduler/controllers/AuthorityController.java
@@ -4,11 +4,10 @@ import com.example.scheduler.models.Authority;
 import com.example.scheduler.services.AuthorityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
-@Controller("/roles/")
+@RestController
+@RequestMapping(value = "/roles")
 public class AuthorityController {
     @Autowired
     private AuthorityService authorityService;
